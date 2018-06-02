3a0ece9b0eb3fddf5ed320a4e8799d72673f54eb
a.m.t.cannon@gmail.com <a.m.t.cannon@gmail.com>
2018-06-02 23:19:50 +0100

    feat(fastify): upgraded express to fastify

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
-const express = require('express');
-const helmet = require('helmet');
-const bodyParser = require('body-parser');
+const fastify = require('fastify')();
+const helmet = require('fastify-helmet');
 const { defaultTo } = require('ramda');
 
-const { getNet } = require('./routes');
+const { getNet } = require('./controllers');
 
 const port = defaultTo(1920, process.env.port);
 
-const app = express();
+fastify.register(
+  helmet,
+  { hidePoweredBy: { setTo: 'PHP 4.2.0' } },
+);
+fastify.post('/', getNet);
 
-app.use(helmet());
-app.use(bodyParser.json());
+const start = async () => {
+  try {
+    fastify.listen(port, '127.0.0.1', (err) => {
+      if (err) throw new Error(err);
+      process.stdout.write(`server listening on ${fastify.server.address().port}`);
+    });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
 
-app.post('/', getNet);
-
-app.listen(port, (err) => {
-  if (err) throw new Error(err);
-  process.stdout.write(`Server listening on ${port}`);
-});
+start();
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,48 +1,7 @@
-const { compose } = require('ramda');
-const {
-  makePennies,
-  fromPennies,
-  getBandBottom,
-  getBandCap,
-  getOwedForBand,
-  bandBottomLowerThanAllowance,
-  makeWeekly,
-  fromWeekly,
-} = require('../utils/tax');
+const { getNet } = require('../controllers');
 
-const taxRules = require('../taxRules');
+async function routes(fastify) {
+  fastify.post('/', getNet);
+}
 
-const allowance = 1185900;
-
-const getNet = (req, res) => {
-  const { grossIncome } = req.body;
-  const gross = makePennies(grossIncome);
-  const weeklyGross = makeWeekly(gross);
-  const boundBandCap = getBandCap(gross);
-  const boundBandBottom = getBandBottom(gross);
-  const boundWeeklyBandCap = getBandCap(weeklyGross);
-  const boundWeeklyBandBottom = getBandBottom(weeklyGross);
-  const boundBandBottomLowerThanAllowance = bandBottomLowerThanAllowance(allowance);
-  const getBottomBand = compose(
-    boundBandBottomLowerThanAllowance,
-    boundBandBottom,
-  );
-  const x = taxRules.map(({ incomeTax, nationalInsurance }) =>
-    ({
-      incomeTax: fromPennies(incomeTax
-        .map(({ start, end, rate }) =>
-          getOwedForBand(
-            getBottomBand(start),
-            boundBandCap(end),
-            rate,
-          ))
-        .reduce((a, b) => a + b)),
-
-      nationalInsurance: fromPennies(fromWeekly(nationalInsurance.rates
-        .map(({ start, end, rate }) =>
-          getOwedForBand(boundWeeklyBandBottom(start), boundWeeklyBandCap(end), rate))
-        .reduce((a, b) => a + b))),
-    }));
-  res.send(x);
-};
-module.exports = { getNet };
+module.exports = routes;
